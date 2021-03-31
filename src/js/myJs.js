1f7c310449c33a75f2564dc8631968f3a0360f7f
TranexTrane <denoncinedouard@hotmail.be>
2021-03-31 16:04:17 +0200

    Push aprem 31-03-21

--- src/js/myJs.js
+++ src/js/myJs.js
@@ -3,3 +3,24 @@ var swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
     },
   });
+
+  let navigation = document.querySelector(".main-nav");
+  let burgerMenu = document.querySelector(".btn-burger")
+  let logo       = document.querySelector(".logo")
+  let remove     = document.querySelector("second")
+        let scrollGoal = (window.innerHeight) / 5;
+        let dernierScroll = 0;
+        window.addEventListener('scroll', function() {
+        let alreadyScrolled = window.pageYOffset;
+        if(alreadyScrolled > scrollGoal) {
+            navigation.classList.add("hide");
+            burgerMenu.classList.add("burger-color");
+        } else {
+            navigation.classList.remove("hide");
+            burgerMenu.classList.remove("burger-color");
+        }
+        // if (alreadyScrolled < dernierScroll) {
+        //     navigation.classList.remove("hide");
+        // }
+        dernierScroll = alreadyScrolled;
+        });
